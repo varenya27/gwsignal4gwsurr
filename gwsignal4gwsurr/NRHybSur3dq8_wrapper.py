3a2af90179db258eda860a51d09763a9d2dad03f
Varenya Upadhyaya <varenyau@gmail.com>
2025-10-13 18:16:15 +0000

    apply time shift to gwsignal polarizations

--- gwsignal4gwsurr/NRHybSur3dq8_wrapper.py
+++ gwsignal4gwsurr/NRHybSur3dq8_wrapper.py
@@ -1,3 +1,4 @@
+import numpy as np
 from astropy import units as u
 from .gwsurr import NRHybSur3dq8_gwsurr
 
@@ -6,7 +7,7 @@ def NRHybSur3dq8_wrapper(freqs, mass1,mass2,spin1z,spin2z,distance,inclination,p
     if waveform_arguments['reference-frequency']<waveform_arguments['f-min']:
         print(f"DBUG fref {waveform_arguments['reference-frequency']} was lower than fmin {waveform_arguments['f-min']}! Setting fref=fmin")
         waveform_arguments['reference-frequency']=waveform_arguments['f-min']
-    hp,hc =  gen.generate_fd_polarizations_from_td(
+    hp_gwsignal,hc_gwsignal =  gen.generate_fd_polarizations_from_td(
         mass1=mass1*u.Msun,
         mass2=mass2*u.Msun,
         spin1z=spin1z*u.dimensionless_unscaled,
@@ -19,5 +20,26 @@ def NRHybSur3dq8_wrapper(freqs, mass1,mass2,spin1z,spin2z,distance,inclination,p
         f_max = max(freqs)*u.Hz,
         deltaF=(freqs[1]-freqs[0])*u.Hz,
     )
-    return {'plus': hp.data, 'cross': hc.data}
+
+    # VU: potential tc fix? cf bilby source.py#L647
+    hp,hc = np.zeros_like(freqs,dtype=complex),np.zeros_like(freqs,dtype=complex)
+    minimum_frequency = waveform_arguments['minimum_frequency']
+    maximum_frequency = waveform_arguments['maximum_frequency']
+    frequency_bounds = ((freqs >= minimum_frequency) * (freqs <= maximum_frequency))
+
+    if len(hp_gwsignal.data.data)>len(freqs):
+        hp = hp_gwsignal.data.data[:len(hp)]
+        hc = hc_gwsignal.data.data[:len(hc)]
+    else:
+        hp[:len(hp_gwsignal.data.data)] = hp_gwsignal.data.data
+        hc[:len(hc_gwsignal.data.data)] = hc_gwsignal.data.data
+    hp *= frequency_bounds
+    hc *= frequency_bounds
+
+    dt = 1/hp_gwsignal.deltaF + (hp_gwsignal.epoch.gpsSeconds + hp_gwsignal.epoch.gpsNanoSeconds*1e-9)
+    time_shift = np.exp(-1j * 2*np.pi * dt * freqs[frequency_bounds])
+    hp[frequency_bounds] *= time_shift
+    hc[frequency_bounds] *= time_shift
+
+    return {'plus': hp, 'cross': hc}
 
