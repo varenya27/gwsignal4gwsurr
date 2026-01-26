0ccf5b5920b537f5e9f81f40f80fdcb3bbdf9f42
Varenya Upadhyaya <varenyau@gmail.com>
2026-01-26 18:56:33 +0000

    add 7dq4 and other testing models

--- gwsignal4gwsurr/gwsurr.py
+++ gwsignal4gwsurr/gwsurr.py
@@ -3,6 +3,7 @@ try:
 except ImportError:
     print("The gwsurrogate package has failed to load, exiting")
 
+from gwtools import harmonics
 import lal
 import numpy as np
 from gwpy.timeseries import TimeSeries
@@ -12,9 +13,10 @@ from lalsimulation.gwsignal.core.waveform import CompactBinaryCoalescenceGenerat
 import lalsimulation.gwsignal.core.gw as gw
 import lalsimulation as lalsim 
 
-# ignore spin magnitude outside training space warnings
+# ignore spin magnitude/mass ratio outside training space warnings
 import warnings
 warnings.filterwarnings('ignore', message='.*Spin')
+warnings.filterwarnings('ignore', message='.*Mass ratio')
 
 
 class NRHybSur3dq8_gwsurr(CompactBinaryCoalescenceGenerator):
@@ -68,19 +70,8 @@ class NRHybSur3dq8_gwsurr(CompactBinaryCoalescenceGenerator):
         dist = self.waveform_dict["distance"]
         q = m1 / m2  # This is the gwsurrogate convention, q=m1/m2>=1
         if q < 1.0:
-            q = 1 / q
-            # VU: swap spins also!
-            chi1,chi2=chi2,chi1
-            # VU: 
-            self.waveform_dict['phi_ref'] += np.pi
+            raise Exception('m2 should not be bigger than m1!')
 
-        # print('------- gwsignal params for td modes generation -------')
-        # print('masses:',m1,m2)
-        # print('spins:',chi1,chi2)
-        # print('time step:',dt)
-        # print('fmin,fref:',fstart,f_ref)
-        # print('distance, inc, phi_ref=',dist/1e6, self.waveform_dict["inclination"], self.waveform_dict["phi_ref"])
-        # print('-------------------------------------------------------')
         times, h, dyn = self.sur(
             q,
             chi1,
@@ -91,6 +82,7 @@ class NRHybSur3dq8_gwsurr(CompactBinaryCoalescenceGenerator):
             units="mks",  # Output in SI units
             M=m1 + m2,  # In solar masses
             dist_mpc=dist/1e6,  # In Mpc
+            # mode_list = [(2,2)]
         )
 
         # gwsurrogate already returns things as a dict
@@ -139,6 +131,7 @@ class NRHybSur3dq8_gwsurr(CompactBinaryCoalescenceGenerator):
         extra_time_fraction = 0.1
         m1_kg = m1 * lal.MSUN_SI
         m2_kg = m2 * lal.MSUN_SI
+
         tchirp = lalsim.SimInspiralChirpTimeBound(fmin, m1_kg, m2_kg, s1z, s2z)
         s = lalsim.SimInspiralFinalBlackHoleSpinBound(s1z,s2z)
         tmerge = lalsim.SimInspiralMergeTimeBound(m1_kg,m2_kg)+lalsim.SimInspiralRingdownTimeBound(m1_kg+m2_kg,s)
@@ -152,11 +145,6 @@ class NRHybSur3dq8_gwsurr(CompactBinaryCoalescenceGenerator):
         # VU: reset this in case it gets used somewhere else
         parameters['f22_start'] = fmin*u.Hz
 
-        # VU: set epoch to merger time according to surrogate convention (instead of start time)
-        epoch = lal.LIGOTimeGPS(
-            hp_.times[np.abs(np.array(hp_.times)).argmin()].value
-        )
-        # VU: maybe not
         epoch = lal.LIGOTimeGPS(
             hp_.times[0].value
         )
@@ -189,12 +177,9 @@ class NRHybSur3dq8_gwsurr(CompactBinaryCoalescenceGenerator):
         else:
             chirplen = int(1.0 / (deltaF * deltaT))
 
-        # print('epoch before resize',hp.epoch)
         # resize waveforms to the required length
         lal.ResizeREAL8TimeSeries(hp, hp.data.length - chirplen, chirplen)
         lal.ResizeREAL8TimeSeries(hc, hc.data.length - chirplen, chirplen)
-        # print('DBUG resizing to:',hp.data.length, hp.data.length-chirplen, chirplen, chirplen/2.+1)
-        # print('epoch after resize',hp.epoch)
 
         # FFT - Using LAL routines
         hptilde = lal.CreateCOMPLEX16FrequencySeries(
@@ -218,16 +203,6 @@ class NRHybSur3dq8_gwsurr(CompactBinaryCoalescenceGenerator):
         lal.REAL8TimeFreqFFT(hctilde, hc, plan)
         lal.REAL8TimeFreqFFT(hptilde, hp, plan)
 
-        # print('------- gwsignal params for fd modes generation -------')
-        # print('length',hptilde.data.length)
-        # print('epoch',hptilde.epoch, hp.epoch)
-        # print('f0',hptilde.f0)
-        # print('sampleUnits',hptilde.sampleUnits)
-        # print('-------------------------------------------------------')
-
-
-
-        # print('DBUG', type(hptilde),hptilde)
         return hptilde, hctilde
        
     def _to_gwpy_series(self, modes_dict, times):
@@ -276,6 +251,143 @@ class NRSur3dq8_Lev2_varenya_gwsurr(NRHybSur3dq8_gwsurr):
             "conditioning_routines" : 'gwsignal'
         }
         return metadata
+    # TODO: add option to add noise to waveform (for marginalization purposes)
+
+    def generate_td_modes(self, **parameters):
+        noisy = parameters.pop('noisy')
+        self.parameter_check(units_sys='Cosmo', **parameters)
+        self.waveform_dict = self._strip_units(self.waveform_dict)
+        fstart, dt = self.waveform_dict["f22_start"], self.waveform_dict["deltaT"]
+        f_ref = self.waveform_dict["f22_ref"]
+
+        m1, m2 = self.waveform_dict["mass1"], self.waveform_dict["mass2"]
+        s1z= self.waveform_dict["spin1z"]
+        s2z= self.waveform_dict["spin2z"]
+        chi1 = np.array( [
+            0.,
+            0.,
+            s1z,
+        ])
+        chi2 = np.array( [
+            0.,
+            0.,
+            s2z,
+        ])
+        dist = self.waveform_dict["distance"]
+        q = m1 / m2  # This is the gwsurrogate convention, q=m1/m2>=1
+        if q < 1.0:
+            raise Exception('m2 should not be bigger than m1!')
+
+        times, h, dyn = self.sur(
+            q,
+            chi1,
+            chi2,
+            dt=dt,
+            f_low=fstart,
+            f_ref=f_ref,
+            units="mks",  # Output in SI units
+            M=m1 + m2,  # In solar masses
+            dist_mpc=dist/1e6,  # In Mpc
+            # mode_list = [(2,2)]
+        )
+
+        
+        if noisy:
+            delta_phi = 1e-4*np.random.randn(len(h[(2,2)]))
+            for ellm, h_array in h.items():
+                ell,m = ellm
+                h[ellm] = h_array*np.exp(1j*m*delta_phi)
+
+        # gwsurrogate already returns things as a dict
+        # indexed by (ell,m) so just return that
+        # Create gwpy timeseries
+        # CK : Adding time_array to h so that this can be passed to
+        # gw.GravitationalWaveModes
+
+        hlm = self._to_gwpy_series(h, times)
+        #hlm['time_array'] = times
+        return gw.GravitationalWaveModes(hlm)
+
+class NRSur3dq8_Lev3_varenya_gwsurr(NRHybSur3dq8_gwsurr):
+    """
+    Implements a toy wrapper for NRSur3dq8_Lev3_varenya in the gwsurrogate package
+
+    Parameters
+    ----------
+
+        No parameters required for initilazation.
+
+    """
+
+    def __init__(self, **kwargs):
+        self.sur = gwsurr.LoadSurrogate("NRSur3dq8_Lev3_varenya")
+        self._update_domains()
+
+    @property
+    def metadata(self):
+        metadata = {
+            "type": "aligned-spin",
+            "f_ref_spin": True,
+            "modes": True,
+            "polarizations": True,
+            "implemented_domain": "time",
+            "approximant" : 'NRSurr',
+            "implementation" : "Python",
+            "conditioning_routines" : 'gwsignal'
+        }
+        return metadata
+    # TODO: add option to add noise to waveform (for marginalization purposes)
+
+    def generate_td_modes(self, **parameters):
+        self.parameter_check(units_sys='Cosmo', **parameters)
+        self.waveform_dict = self._strip_units(self.waveform_dict)
+        fstart, dt = self.waveform_dict["f22_start"], self.waveform_dict["deltaT"]
+        f_ref = self.waveform_dict["f22_ref"]
+
+        m1, m2 = self.waveform_dict["mass1"], self.waveform_dict["mass2"]
+        s1z= self.waveform_dict["spin1z"]
+        s2z= self.waveform_dict["spin2z"]
+        chi1 = np.array( [
+            0.,
+            0.,
+            s1z,
+        ])
+        chi2 = np.array( [
+            0.,
+            0.,
+            s2z,
+        ])
+        dist = self.waveform_dict["distance"]
+        q = m1 / m2  # This is the gwsurrogate convention, q=m1/m2>=1
+        if q < 1.0:
+            raise Exception('m2 should not be bigger than m1!')
+
+        times, h, dyn = self.sur(
+            q,
+            chi1,
+            chi2,
+            dt=dt,
+            f_low=fstart,
+            f_ref=f_ref,
+            units="mks",  # Output in SI units
+            M=m1 + m2,  # In solar masses
+            dist_mpc=dist/1e6,  # In Mpc
+            mode_list = [(2,2)]
+        )
+        # delta_phi = 1e-4*np.random.randn(len(h[(2,2)]))
+        # for ellm, h_array in h.items():
+        #     ell,m = ellm
+        #     h[ellm] = h_array*np.exp(1j*m*delta_phi)
+
+        # gwsurrogate already returns things as a dict
+        # indexed by (ell,m) so just return that
+        # Create gwpy timeseries
+        # CK : Adding time_array to h so that this can be passed to
+        # gw.GravitationalWaveModes
+
+        hlm = self._to_gwpy_series(h, times)
+        #hlm['time_array'] = times
+        return gw.GravitationalWaveModes(hlm)
 
 # VU: Not tested/properly implemented yet, proceed with caution
 class NRSur7dq4_gwsurr(CompactBinaryCoalescenceGenerator):
@@ -314,36 +426,35 @@ class NRSur7dq4_gwsurr(CompactBinaryCoalescenceGenerator):
     def generate_td_modes(self, **parameters):
         self.parameter_check(units_sys='Cosmo', **parameters)
         self.waveform_dict = self._strip_units(self.waveform_dict)
-        fmin, dt = self.waveform_dict["f22_start"], self.waveform_dict["deltaT"]
+        f_start, dt = self.waveform_dict["f22_start"], self.waveform_dict["deltaT"]
+        f_ref = self.waveform_dict["f22_ref"]
+
         m1, m2 = self.waveform_dict["mass1"], self.waveform_dict["mass2"]
-        chi1 = np.array(
-            [
-                self.waveform_dict["spin1x"],
-                self.waveform_dict["spin1y"],
-                self.waveform_dict["spin1z"],
-            ]
-        )
-        chi2 = np.array(
-            [
-                self.waveform_dict["spin2x"],
-                self.waveform_dict["spin2y"],
-                self.waveform_dict["spin2z"],
-            ]
-        )
+        chi1 = np.array( [
+            self.waveform_dict["spin1x"],
+            self.waveform_dict["spin1y"],
+            self.waveform_dict["spin1z"],
+        ])
+        chi2 = np.array( [
+            self.waveform_dict["spin2x"],
+            self.waveform_dict["spin2y"],
+            self.waveform_dict["spin2z"],
+        ])
         dist = self.waveform_dict["distance"]
         q = m1 / m2  # This is the gwsurrogate convention, q=m1/m2>=1
         if q < 1.0:
-            q = 1 / q
+            raise Exception('m2 should not be bigger than m1!')
+
         times, h, dyn = self.sur(
             q,
             chi1,
             chi2,
             dt=dt,
-            f_low=fmin,
-            f_ref=fmin,
+            f_low=f_start,
+            f_ref=f_ref,
             units="mks",  # Output in SI units
             M=m1 + m2,  # In solar masses
-            dist_mpc=dist/1e6  # In Mpc
+            dist_mpc=dist/1e6,  # In Mpc
         )
 
         # gwsurrogate already returns things as a dict
@@ -356,15 +467,95 @@ class NRSur7dq4_gwsurr(CompactBinaryCoalescenceGenerator):
         #hlm['time_array'] = times
         return gw.GravitationalWaveModes(hlm)
 
-
     def generate_td_waveform(self, **parameters):
-        # VU: added pi/2-phi_ref to match LALSuite convention
+        # VU: added pi/2-phi_ref to match LALSim convention
         theta, phi = parameters['inclination'], (np.pi/2-parameters['phi_ref'].value)*u.rad
         hlm = self.generate_td_modes(**parameters)
         hp, hc = hlm(theta, phi)
         hp, hc = TimeSeries(hp, name='hplus'), TimeSeries(hc, name='hcross')
         return hp, hc
 
+    def generate_fd_polarizations_from_td(self, **parameters):
+        # VU: inspired by LALSimInspiralGeneratorConditioning.c L486
+        # Adjust deltaT depending on sampling rate
+        fmax = parameters["f_max"].value
+        f_nyquist = fmax
+        deltaF = 0
+        if "deltaF" in parameters.keys():
+            deltaF = parameters["deltaF"].value
+
+        if deltaF != 0:
+            n = int(np.round(fmax / deltaF))
+            if n & (n - 1):
+                chirplen_exp = np.frexp(n)
+                f_nyquist = np.ldexp(1, int(chirplen_exp[1])) * deltaF
+
+        deltaT = 0.5 / f_nyquist
+        parameters["deltaT"] = deltaT*u.s
+
+
+        hp_,hc_ = self.generate_td_waveform(**parameters)
+
+
+        epoch = lal.LIGOTimeGPS(
+            hp_.times[0].value
+        )
+
+        hp = lal.CreateREAL8TimeSeries(
+            "hplus", epoch, 0, parameters["deltaT"].value, lal.DimensionlessUnit, len(hp_)
+        )
+        hc = lal.CreateREAL8TimeSeries(
+            "hcross", epoch, 0, parameters["deltaT"].value, lal.DimensionlessUnit, len(hc_),
+        )
+
+        hp.data.data = hp_.value
+        hc.data.data = hc_.value
+
+        # conditioning/tapering is done differently since this is a short waveform 
+        # [cf. L#44 in LALSimInspiralGeneratorConditioning.c]
+        taper = True
+        lalsim.SimInspiralREAL8WaveTaper(hp.data,taper)
+        lalsim.SimInspiralREAL8WaveTaper(hc.data,taper)
+        
+        # Adjust signal duration
+        if deltaF == 0:
+            chirplen = hp.data.length
+            chirplen_exp = np.frexp(chirplen)
+            chirplen = int(np.ldexp(1, chirplen_exp[1]))
+            deltaF = 1.0 / (chirplen * deltaT)
+            parameters["deltaF"] = deltaF
+
+        else:
+            chirplen = int(1.0 / (deltaF * deltaT))
+
+        # resize waveforms to the required length
+        lal.ResizeREAL8TimeSeries(hp, hp.data.length - chirplen, chirplen)
+        lal.ResizeREAL8TimeSeries(hc, hc.data.length - chirplen, chirplen)
+
+        # FFT - Using LAL routines
+        hptilde = lal.CreateCOMPLEX16FrequencySeries(
+            "FD H_PLUS",
+            hp.epoch,
+            0.0,
+            deltaF,
+            lal.DimensionlessUnit,
+            int(chirplen / 2.0 + 1),
+        )
+        hctilde = lal.CreateCOMPLEX16FrequencySeries(
+            "FD H_CROSS",
+            hc.epoch,
+            0.0,
+            deltaF,
+            lal.DimensionlessUnit,
+            int(chirplen / 2.0 + 1),
+        )
+
+        plan = lal.CreateForwardREAL8FFTPlan(chirplen, 0)
+        lal.REAL8TimeFreqFFT(hctilde, hc, plan)
+        lal.REAL8TimeFreqFFT(hptilde, hp, plan)
+
+        return hptilde, hctilde
+       
     def _to_gwpy_series(self, modes_dict, times):
         """
         Iterate over the dict and return a dict of gwpy TimeSeries objects
@@ -373,7 +564,6 @@ class NRSur7dq4_gwsurr(CompactBinaryCoalescenceGenerator):
         for ellm, mode in modes_dict.items():
             gwpy_dict[ellm] = TimeSeries(mode, times=times, name='h_%i_%i'%(ellm[0], ellm[1]))
         return gwpy_dict
-
 
     def _strip_units(self, waveform_dict):
         new_dc = {}
